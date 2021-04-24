c6da7adaf30c788ea5b63311e43112f0677667a0
Vzor <S0k0linA@yandex.ru>
2021-04-24 16:05:01 +0300

    Исправление замечаний по декоративным элементам

--- source/js/script.js
+++ source/js/script.js
@@ -19,24 +19,34 @@ if (document.querySelector('.slider')) {
   const sliderToggle = document.querySelector('.slider__toggle');
   const toggleBefore = document.querySelector('.slider__button--before');
   const toggleAfter = document.querySelector('.slider__button--after');
+  const imageBefore = document.querySelector('.slider__image--before');
+  const imageAfter = document.querySelector('.slider__image--after');
 
   sliderToggle.addEventListener('click',function () {
     if (sliderToggle.classList.contains('slider__toggle--before')){
       sliderToggle.classList.remove('slider__toggle--before');
       sliderToggle.classList.add('slider__toggle--after');
+      imageAfter.classList.add('slider__image--active');
+      imageBefore.classList.remove('slider__image--active');
     } else {
       sliderToggle.classList.add('slider__toggle--before');
       sliderToggle.classList.remove('slider__toggle--after');
+      imageAfter.classList.remove('slider__image--active');
+      imageBefore.classList.add('slider__image--active');
     }
   });
 
   toggleBefore.addEventListener('click',function () {
     sliderToggle.classList.add('slider__toggle--before');
     sliderToggle.classList.remove('slider__toggle--after');
+    imageBefore.classList.add('slider__image--active');
+    imageAfter.classList.remove('slider__image--active');
   });
 
   toggleAfter.addEventListener('click',function () {
     sliderToggle.classList.remove('slider__toggle--before');
     sliderToggle.classList.add('slider__toggle--after');
+    imageAfter.classList.add('slider__image--active');
+    imageBefore.classList.remove('slider__image--active');
   });
 }
